c96a230bd2a73cb51019123e47c37a8f51a01939
benbenlaw <benlawrenceben@hotmail.co.uk>
2026-03-26 14:35:31 +0000

    12.1.2

---
+++ src/main/java/com/benbenlaw/core/util/FluidRendererUtil.java
@@ -0,0 +1,61 @@
+package com.benbenlaw.core.util;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.block.FluidModel;
+import net.minecraft.client.renderer.rendertype.RenderType;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.core.Direction;
+import net.minecraft.data.AtlasIds;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import net.neoforged.neoforge.client.extensions.common.IClientFluidTypeExtensions;
+import net.neoforged.neoforge.fluids.FluidStack;
+
+public class FluidRendererUtil {
+
+    //FROM ENDER IO UNDER https://github.com/Team-EnderIO/EnderIO/blob/26.1/LICENSE.txt//
+
+    public static void submitFluid(PoseStack poseStack, RenderType renderType, SubmitNodeCollector nodeCollector, FluidStack fluidStack,
+        float fillAmount, int packedLight) {
+        if (fluidStack.isEmpty()) return;
+
+        Fluid fluid = fluidStack.getFluid();
+        submitFluid(poseStack, renderType, nodeCollector, fluid, fillAmount, packedLight);
+    }
+
+    public static void submitFluid(PoseStack poseStack, RenderType renderType, SubmitNodeCollector nodeCollector, Fluid fluid, float fillAmount, int packedLight) {
+        // Get fluid model
+        FluidState fluidState = fluid.defaultFluidState();
+        FluidModel fluidModel = Minecraft.getInstance().getModelManager().getFluidStateModelSet().get(fluidState);
+
+        // Get tint color
+        int color = 0xFFFFFFFF;
+        if (fluidModel.fluidTintSource() != null) {
+            color = fluidModel.fluidTintSource().color(fluidState);
+        }
+
+        // Get fluid texture
+        TextureAtlasSprite texture = fluidModel.stillMaterial().sprite();
+
+        // Get sizes
+        float fluidHeight = (14 * fillAmount) / 16.0f;
+        float inset = 0.0625F;
+        float faceSize = 14 / 16.0f;
+
+        // Top
+        RenderUtil.submitFace(Direction.UP, poseStack, renderType, nodeCollector, texture, inset, inset, inset + fluidHeight, faceSize,
+            faceSize, color, packedLight);
+
+        // Sides
+        RenderUtil.submitFace(Direction.SOUTH, poseStack, renderType, nodeCollector, texture, inset, inset, inset, faceSize, fluidHeight,
+            color, packedLight);
+        RenderUtil.submitFace(Direction.NORTH, poseStack, renderType, nodeCollector, texture, inset, inset, inset, faceSize, fluidHeight,
+            color, packedLight);
+        RenderUtil.submitFace(Direction.EAST, poseStack, renderType, nodeCollector, texture, inset, inset, inset, faceSize, fluidHeight,
+            color, packedLight);
+        RenderUtil.submitFace(Direction.WEST, poseStack, renderType, nodeCollector, texture, inset, inset, inset, faceSize, fluidHeight,
+            color, packedLight);
+    }
+}
--- src/main/java/com/benbenlaw/core/util/RenderUtil.java
+++ src/main/java/com/benbenlaw/core/util/RenderUtil.java
@@ -1,104 +1,140 @@
 package com.benbenlaw.core.util;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.Direction;
-import net.minecraft.util.ARGB;
 import net.minecraft.util.LightCoordsUtil;
-import org.joml.Vector3f;
 
 public class RenderUtil {
 
-    // Render a face with default full brightness
-    public static void renderFace(Direction face, PoseStack.Pose pose, VertexConsumer consumer,
-                                  TextureAtlasSprite texture, float x, float y, float z, float w, float h, int color) {
-        renderFace(face, pose, consumer, texture, x, y, z, w, h, color, LightCoordsUtil.FULL_BRIGHT);
+    //FROM ENDER IO UNDER https://github.com/Team-EnderIO/EnderIO/blob/26.1/LICENSE.txt//
+
+    /**
+     * Render a face with its texture with local face coordinates.
+     * Note: Up and Down UVs may not be accurate, please PR with a fix (and test all uses of this method) to modify it.
+     * todo; is this confusing?
+     */
+    public static void submitFace(Direction face, PoseStack poseStack, RenderType renderType, SubmitNodeCollector nodeCollector,
+            TextureAtlasSprite texture, float x, float y, float z, float w, float h, int color) {
+        submitFace(face, poseStack, renderType, nodeCollector, texture, x, y, z, w, h, color, LightCoordsUtil.FULL_BRIGHT);
     }
 
-    // Render a face with specified light
-    public static void renderFace(Direction face, PoseStack.Pose pose, VertexConsumer consumer,
-                                  TextureAtlasSprite texture, float x, float y, float z, float w, float h, int color, int light) {
+    public static void submitFace(Direction face, PoseStack poseStack, RenderType renderType, SubmitNodeCollector nodeCollector,
+            TextureAtlasSprite texture, float x, float y, float z, float w, float h, int color, int light) {
+        // Normals are taken from Direction enum. They are necessary for proper lighting
+        // and block breaking textures
         switch (face) {
-            case DOWN -> renderFace(pose, consumer, texture, color, light,
-                    x, x + w, y, y, z, z, z, z, x, x + w, y, y, 0, -1, 0);
-            case UP -> renderFace(pose, consumer, texture, color, light,
-                    x, x + w, y + h, y + h, z, z, z, z, x, x + w, y, y, 0, 1, 0);
-            case NORTH -> renderFace(pose, consumer, texture, color, light,
-                    x, x + w, y + h, y, z, z, z, z, x, x + w, y, y + h, 0, 0, -1);
-            case SOUTH -> renderFace(pose, consumer, texture, color, light,
-                    x, x + w, y, y + h, z, z, z, z, x + w, x, y + h, y, 0, 0, 1);
-            case EAST -> renderFace(pose, consumer, texture, color, light,
-                    x, x, y + h, y, z, z + w, z + w, z, z, z + w, y, y + h, 1, 0, 0);
-            case WEST -> renderFace(pose, consumer, texture, color, light,
-                    x, x, y, y + h, z, z + w, z + w, z, x + w, x, y + h, y, -1, 0, 0);
+        case DOWN -> submitFace(poseStack, renderType, nodeCollector, texture, color, light, x, x + w, 1.0f - z, 1.0f - z, y, y, y + h, y + h,
+                x, x + w, y, y + h, 0, -1, 0);
+        case UP -> submitFace(poseStack, renderType, nodeCollector, texture, color, light, x, x + w, z, z, y + h, y + h, y, y, x, x + w, y,
+                y + h, 0, 1, 0);
+        case NORTH -> submitFace(poseStack, renderType, nodeCollector, texture, color, light, x, x + w, y + h, y, z, z, z, z, x, x + w, y,
+                y + h, 0, 0, -1);
+        case SOUTH -> submitFace(poseStack, renderType, nodeCollector, texture, color, light, x, x + w, y, y + h, 1.0f - z, 1.0f - z,
+                1.0f - z, 1.0f - z, x + w, x, y + h, y, 0, 0, 1);
+        case EAST -> submitFace(poseStack, renderType, nodeCollector, texture, color, light, 1.0f - z, 1.0f - z, y + h, y, x, x + w, x + w, x,
+                x, x + w, y, y + h, 1, 0, 0);
+        case WEST -> submitFace(poseStack, renderType, nodeCollector, texture, color, light, z, z, y, y + h, x, x + w, x + w, x, x + w, x,
+                y + h, y, -1, 0, 0);
+        default -> throw new IllegalStateException("Unexpected value: " + face);
         }
     }
 
-    // Core face renderer
-// Core face renderer
-    private static void renderFace(PoseStack.Pose pose, VertexConsumer consumer, TextureAtlasSprite texture,
-                                   int color, int light,
-                                   float x0, float x1, float y0, float y1,
-                                   float z0, float z1, float z2, float z3,
-                                   float u0, float u1, float v0, float v1,
-                                   float normalX, float normalY, float normalZ) {
+    private static void submitFace(PoseStack pose, RenderType renderType, SubmitNodeCollector nodeCollector, TextureAtlasSprite texture, int color,
+            int light, float x0, float x1, float y0, float y1, float z0, float z1, float z2, float z3, float minU,
+            float maxU, float minV, float maxV, float normalX, float normalY, float normalZ) {
 
-        float minU = texture.getU(u0);
-        float maxU = texture.getU(u1);
-        float minV = texture.getV(v0);
-        float maxV = texture.getV(v1);
-
-        consumer.addVertex(x0, y0, z0)
+        nodeCollector.submitCustomGeometry(pose, renderType, (pose1, consumer) -> {
+            consumer.addVertex(pose1, x0, y0, z0)
                 .setColor(color)
-                .setUv(minU, minV)
+                .setUv(texture.getU(minU), texture.getV(minV))
                 .setOverlay(OverlayTexture.NO_OVERLAY)
                 .setLight(light)
-                .setNormal(pose, normalX, normalY, normalZ);
-
-        consumer.addVertex(x1, y0, z1)
+                .setNormal(pose1, normalX, normalY, normalZ);
+            consumer.addVertex(pose1, x1, y0, z1)
                 .setColor(color)
-                .setUv(maxU, minV)
+                .setUv(texture.getU(maxU), texture.getV(minV))
                 .setOverlay(OverlayTexture.NO_OVERLAY)
                 .setLight(light)
-                .setNormal(pose, normalX, normalY, normalZ);
-
-        consumer.addVertex(x1, y1, z2)
+                .setNormal(pose1, normalX, normalY, normalZ);
+            consumer.addVertex(pose1, x1, y1, z2)
                 .setColor(color)
-                .setUv(maxU, maxV)
+                .setUv(texture.getU(maxU), texture.getV(maxV))
                 .setOverlay(OverlayTexture.NO_OVERLAY)
                 .setLight(light)
-                .setNormal(pose, normalX, normalY, normalZ);
-
-        consumer.addVertex(x0, y1, z3)
+                .setNormal(pose1, normalX, normalY, normalZ);
+            consumer.addVertex(pose1, x0, y1, z3)
                 .setColor(color)
-                .setUv(minU, maxV)
+                .setUv(texture.getU(minU), texture.getV(maxV))
                 .setOverlay(OverlayTexture.NO_OVERLAY)
                 .setLight(light)
-                .setNormal(pose, normalX, normalY, normalZ);
+                .setNormal(pose1, normalX, normalY, normalZ);
+        });
     }
 
+//    public static float[] unpackVertices(int[] vertices, int vertexIndex, int position, int count) {
+//        float[] floats = new float[count];
+//        int startIndex = vertexIndex * IQuadTransformer.STRIDE + position;
+//        for (int i = 0; i < count; i++) {
+//            floats[i] = Float.intBitsToFloat(vertices[startIndex + i]);
+//        }
+//        return floats;
+//    }
 
-    // ARGB helpers
-    public static float getRed(int color) {
-        return (float) ARGB.red(color) / 255.0F;
+//    public static Vector3f getNormalData(int[] vertices, int vertexIndex) {
+//        float x;
+//        float y;
+//        float z;
+//
+//        int vertexData = vertices[vertexIndex * STRIDE + IQuadTransformer.NORMAL];
+//        x = (vertexData & 0x000000FF) / 127f;
+//        y = ((vertexData & 0x0000FF00) >> 8) / 127f;
+//        z = ((vertexData & 0x00FF0000) >> 16) / 127f;
+//        return new Vector3f(x, y, z);
+//    }
+
+    /**
+     * {@see net.neoforged.neoforge.client.model.pipeline.QuadBakingVertexConsumer}
+     * @return packedUV Data
+     */
+    public static int[] packUV(float u, float v) {
+        int[] quadData = new int[2];
+        quadData[0] = Float.floatToRawIntBits(u);
+        quadData[1] = Float.floatToRawIntBits(v);
+        return quadData;
     }
 
-    public static float getGreen(int color) {
-        return (float) ARGB.green(color) / 255.0F;
+//    private static int[] getColorABGR(int[] vertices, int vertexIndex) {
+//        int color = vertices[STRIDE * vertexIndex + COLOR];
+//        int[] abgr = new int[4];
+//        abgr[0] = color >> 24 & 0xFF;
+//        abgr[1] = color >> 16 & 0xFF;
+//        abgr[2] = color >> 8 & 0xFF;
+//        abgr[3] = color & 0xFF;
+//        return abgr;
+//    }
+
+    private static int[] multiplyColor(int[] abgr1, int[] abgr2) {
+        return new int[] { abgr1[0] * abgr2[0] / 255, abgr1[1] * abgr2[1] / 255, abgr1[2] * abgr2[2] / 255,
+                abgr1[3] * abgr2[3] / 255 };
     }
 
-    public static float getBlue(int color) {
-        return (float) ARGB.blue(color) / 255.0F;
-    }
+//    public static void multiplyColor(int[] vertices, int vertexIndex, int rgbBlockColor) {
+//        int[] colorABGR = RenderUtil.getColorABGR(vertices, vertexIndex);
+//        int[] blockColorABGR = new int[4];
+//        blockColorABGR[0] = 0xFF | (rgbBlockColor >> 24 & 0xFF);
+//        blockColorABGR[3] = rgbBlockColor >> 16 & 0xFF;
+//        blockColorABGR[2] = rgbBlockColor >> 8 & 0xFF;
+//        blockColorABGR[1] = rgbBlockColor & 0xFF;
+//        int[] multipliedColor = RenderUtil.multiplyColor(colorABGR, blockColorABGR);
+//        RenderUtil.putColorABGR(vertices, vertexIndex, multipliedColor);
+//    }
 
-    public static float getAlpha(int color) {
-        return (float) ARGB.alpha(color) / 255.0F;
-    }
-
-    // Pack ARGB into int (optional)
-    public static int packARGB(int alpha, int red, int green, int blue) {
-        return (alpha & 0xFF) << 24 | (red & 0xFF) << 16 | (green & 0xFF) << 8 | (blue & 0xFF);
-    }
+//    public static void putColorABGR(int[] vertices, int vertexIndex, int[] abgr) {
+//        int offset = vertexIndex * STRIDE + COLOR;
+//        vertices[offset] = (abgr[0] << 24) | (abgr[1] << 16) | (abgr[2] << 8) | abgr[3];
+//    }
 }
